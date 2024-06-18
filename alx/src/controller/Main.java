60ea0c89fe5d1f3a9c8854df9bd25d5fdb56322f
Caetano Santana <caetano.m@ufms.br>
2024-06-18 12:48:24 -0400

    remove databasesetup

--- alx/src/controller/Main.java
+++ alx/src/controller/Main.java
@@ -11,18 +11,11 @@ import model.Livro;
 
 public class Main {
 
-    // URL de conexão com o banco de dados SQLite
-    static String url = "jdbc:sqlite:library.db";
-
     public static void main(String[] args) {
 
-       //DatabaseSetup.setupDatabase();
-
         // Livros
-        Livro livro1 = new Livro("12238-3-16-148410-0", "Editora ABC", "Autor A", "Todo mundo odeia Java",
-                "From Basics to Advanced", "Programming", 50);
-        Livro livro2 = new Livro("978-85-950545-2-7", "Editora XYZ", "Autor B", "Desvendando os Segredos do Python",
-                "Aprenda a programar do zero", "Programação", 720);
+        Livro livro1 = new Livro("12238-3-16-148410-0", "Editora ABC", "Autor A", "Todo mundo odeia Java","From Basics to Advanced", "Programming", 50);
+        Livro livro2 = new Livro("978-85-950545-2-7", "Editora XYZ", "Autor B", "Desvendando os Segredos do Python","Aprenda a programar do zero", "Programação", 720);
         Livro livro3 = new Livro("3-446-39819-X", "Editora LTDA", "Autores C e D","Algoritmos e Estruturas de Dados em Java", "Fundamentos para Programadores", "Programação", 610);
 
         // Instância do LivroDAO
