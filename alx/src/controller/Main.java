e270f66a9a43ad93e878e5c7554f70d6f34d3382
Caetano Santana <caetano.m@ufms.br>
2024-06-18 14:11:55 -0400

    corrige buscaCliente

--- alx/src/controller/Main.java
+++ alx/src/controller/Main.java
@@ -11,53 +11,43 @@ import model.Livro;
 
 public class Main {
 
+    // URL de conexão com o banco de dados SQLite
+    static String url = "jdbc:sqlite:library.db";
+
     public static void main(String[] args) {
 
-        // Livros
-        Livro livro1 = new Livro("12238-3-16-148410-0", "Editora ABC", "Autor A", "Todo mundo odeia Java","From Basics to Advanced", "Programming", 50);
-        Livro livro2 = new Livro("978-85-950545-2-7", "Editora XYZ", "Autor B", "Desvendando os Segredos do Python","Aprenda a programar do zero", "Programação", 720);
-        Livro livro3 = new Livro("3-446-39819-X", "Editora LTDA", "Autores C e D","Algoritmos e Estruturas de Dados em Java", "Fundamentos para Programadores", "Programação", 610);
 
-        // Instância do LivroDAO
+        // Livro livro1 = new Livro("12238-3-16-148410-0", "Editora ABC", "Autor A", "Todo mundo odeia Java","From Basics to Advanced", "Programming", 50);
+        // Livro livro2 = new Livro("978-85-950545-2-7", "Editora XYZ", "Autor B", "Desvendando os Segredos do Python","Aprenda a programar do zero", "Programação", 720);
+        // Livro livro3 = new Livro("3-446-39819-X", "Editora LTDA", "Autores C e D", "Algoritmos e Estruturas de Dados em Java", "Fundamentos para Programadores", "Programação", 610);
+        // Exemplar exemplar1 = new Exemplar("Disponivel", 1, 101, 10, livro1);
+        // Exemplar exemplar2 = new Exemplar("Disponivel", 2, 102, 20, livro2);
+        // Cliente cliente1 = new Cliente("12345678901", "Ana");
+        // Cliente cliente2 = new Cliente("23456789012", "Bruno");
+        // Cliente cliente3 = new Cliente("34567890123", "Carla");
+        // Funcionario funcionario1 = new Funcionario("45678901234", "David", "Gerente", true);
+        // Funcionario funcionario2 = new Funcionario("56789012345", "Eva", "Assistente", false);
+
+
         LivroDAO livroDAO = new LivroDAO();
-
-        // Cadastrar o livro
-        livroDAO.salvarLivro(livro1);
-        livroDAO.salvarLivro(livro2);
-        livroDAO.salvarLivro(livro3);
-
-        // Dados dos exemplares a serem cadastrados
-        Exemplar exemplar1 = new Exemplar("Disponivel", 1, 101, 10, livro1);
-        Exemplar exemplar2 = new Exemplar("Disponivel", 2, 102, 20, livro2);
-
-        // Cadastrar os exemplares
-        livroDAO.salvarExemplar(exemplar1);
-        livroDAO.salvarExemplar(exemplar2);
-
-        Cliente cliente1 = new Cliente("12345678901", "Ana");
-        Cliente cliente2 = new Cliente("23456789012", "Bruno");
-        Cliente cliente3 = new Cliente("34567890123", "Carla");
-
-        // Instância do ClienteDAO
         ClienteDAO clienteDAO = new ClienteDAO();
-
-        // Cadastrar os clientes
-        clienteDAO.salvaCliente(cliente1);
-        clienteDAO.salvaCliente(cliente2);
-        clienteDAO.salvaCliente(cliente3);
-
-        Funcionario funcionario1 = new Funcionario("45678901234", "David", "Gerente", true);
-        Funcionario funcionario2 = new Funcionario("56789012345", "Eva", "Assistente", false);
-
-        // Instância do FuncionarioDAO
         FuncionarioDAO funcionarioDAO = new FuncionarioDAO();
 
-        // Cadastrar os funcionários
-        funcionarioDAO.salvarFuncionario(funcionario1);
-        funcionarioDAO.salvarFuncionario(funcionario2);
 
-        // Buscar funcionários
-        String cpfBuscaFuncionario = "45678901234";
+        // livroDAO.salvarLivro(livro1);
+        // livroDAO.salvarLivro(livro2);
+        // livroDAO.salvarLivro(livro3);
+        // livroDAO.salvarExemplar(exemplar1);
+        // livroDAO.salvarExemplar(exemplar2);
+        // clienteDAO.salvaCliente(cliente1);
+        // clienteDAO.salvaCliente(cliente2);
+        // clienteDAO.salvaCliente(cliente3);
+        // funcionarioDAO.salvarFuncionario(funcionario1);
+        // funcionarioDAO.salvarFuncionario(funcionario2);
+       
+
+        Controladora controladora = new Controladora();
+        String cpfBuscaFuncionario = "12345678927";
         Funcionario funcionarioEncontrado = funcionarioDAO.buscaFuncionario(cpfBuscaFuncionario);
 
         if (funcionarioEncontrado != null) {
@@ -69,32 +59,35 @@ public class Main {
         }
 
         // Buscar cliente para iniciar empréstimo
-        String cpfBuscaCliente = "12345678901"; // CPF que você quer buscar
+        String cpfBuscaCliente = "12345678901";
 
         Cliente clienteEncontrado = clienteDAO.buscaCliente(cpfBuscaCliente);
-
-        Controladora controladora = new Controladora();
-
-        // Verificar se cliente é apto para iniciar empréstimo
-        controladora.iniciarEmprestimo(clienteEncontrado);
 
         if (clienteEncontrado != null) {
             System.out.println("Cliente encontrado: ");
             System.out.println("CPF: " + clienteEncontrado.getCpf());
             System.out.println("Nome: " + clienteEncontrado.getNome());
             System.out.println("Data Cadastro: " + clienteEncontrado.getDataCadastro());
+            controladora.iniciarEmprestimo(clienteEncontrado);
         } else {
             System.out.println("Cliente com CPF " + cpfBuscaCliente + " não encontrado.");
         }
 
+      
+
+        // Verificar se cliente é apto para iniciar empréstimo
+        
+
+        
+
         // Simular registro de empréstimo utilizando o ID gerado pelo banco de dados
         int idEmprestimo = 1; // Suponha que o ID do empréstimo seja 1 (gerado pelo banco de dados)
 
-        controladora.iniciarEmprestimo(cliente1); // Iniciar o empréstimo
+        //controladora.iniciarEmprestimo(cliente1); // Iniciar o empréstimo
 
         // Verificar se o empréstimo foi iniciado antes de registrar
-        controladora.registrarEmprestimo(idEmprestimo, cliente1);
-       
+        //controladora.registrarEmprestimo(idEmprestimo, cliente1);
+
     }
 
 }
--- alx/src/dao/ClienteDAO.java
+++ alx/src/dao/ClienteDAO.java
@@ -5,30 +5,47 @@ import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 import model.Cliente;
 import model.Emprestimo;
 
 public class ClienteDAO {
   private static final String url = "jdbc:sqlite:library.db";
+  private static final SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
 
   public Cliente buscaCliente(String cpf) {
-    String sqlBuscaPessoa = "SELECT p.CPF, p.nome, c.dataCadastro " +
-        "FROM Pessoa p " +
-        "INNER JOIN Cliente c ON p.ID = c.ID_cliente " +
-        "WHERE p.CPF = ?";
+
+    String sqlBuscaCliente = "SELECT p.CPF, p.nome, c.dataCadastro " +
+    "FROM Pessoa p " +
+    "JOIN Cliente c ON p.ID = c.ID_cliente " +
+    "WHERE p.CPF = ?";
+
     try (Connection conn = DriverManager.getConnection(url);
-        PreparedStatement pstmtBuscaPessoa = conn.prepareStatement(sqlBuscaPessoa)) {
+        PreparedStatement pstmtBuscaPessoa = conn.prepareStatement(sqlBuscaCliente)) {
 
       pstmtBuscaPessoa.setString(1, cpf);
-      ResultSet rsBusca = pstmtBuscaPessoa.executeQuery();
+      ResultSet result = pstmtBuscaPessoa.executeQuery();
 
-      if (rsBusca.next()) {
-        Cliente cliente = new Cliente(rsBusca.getString("CPF"), rsBusca.getString("nome"));
-        cliente.setDataCadastro(rsBusca.getDate("dataCadastro"));
+      if (result.next()) {
+        String nome = result.getString("nome");
+        String cpfString = result.getString("cpf");
+        String dataString = result.getString("dataCadastro");
 
+        Cliente cliente = new Cliente(cpfString, nome);
+
+        Date dataCadastro = null;
+        try {
+            dataCadastro = dateFormat.parse(dataString);
+        } catch (ParseException e) {
+            System.err.println("Erro ao converter data: " + e.getMessage());
+        }
+        cliente.setDataCadastro(dataCadastro);
         return cliente;
       }
+      
     } catch (SQLException e) {
       System.err.println("Erro ao buscar cliente: " + e.getMessage());
     }
